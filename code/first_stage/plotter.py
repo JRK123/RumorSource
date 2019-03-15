01f08351b9ed6b3d3a6ec8b508a506607274b631
ganeshlandge <ganeshlandge936@gmail.com>
2019-03-15 10:01:33 +0530

    Update plotter.py

--- code/first_stage/plotter.py
+++ code/first_stage/plotter.py
@@ -7,7 +7,8 @@ from operator import itemgetter
 import glob
 from mle import mle_cal
 
-def prob():  # CALCULATES PROBABILITY OF EACH INSTANCE
+	#------------------CALCULATES PROBABILITY OF EACH INSTANCE----------------
+def prob():  
 	W = [[] for i in range(4)]
 	for i in range(4):
 		with open('instance' + str(i+1) + '.txt') as file:
@@ -70,16 +71,16 @@ for item in files:
 			adj[int(src)].append(int(dest))  				# add destination to the index of src 
 			adj[int(dest)].append(int(src))  				# add source to the index at dest
 		for j in range(1,len(adj)): 					# j-> 1 to 35 
-			flag = 0 										# set flag to 0
-			for k in range(0,len(adj[j])):					# k -> 0 to no. of neighbours of j
-				if(dct[j] != dct[adj[j][k]]):					# if colour of j is not equal to colour of any of its neighbour		
+			flag = 0 						# set flag to 0
+			for k in range(0,len(adj[j])):				# k -> 0 to no. of neighbours of j
+				if(dct[j] != dct[adj[j][k]]):			# if colour of j is not equal to colour of any of its neighbour		
 					flag = 1										# then set flag as 1
 					break;											# break the loop
-			if(flag == 0):									# if all neighbouring colours are same						
-				for p in range(0,len(adj[j])):					# p-> 0 to no. of neighbours of j
-					adj[adj[j][p]].remove(j)						# remove j from the neighbour's list of links
-				adj[j] = []										# empty j
-		#print(adj)										# return adj
+			if(flag == 0):						# if all neighbouring colours are same						
+				for p in range(0,len(adj[j])):			# p-> 0 to no. of neighbours of j
+					adj[adj[j][p]].remove(j)		# remove j from the neighbour's list of links
+				adj[j] = []					# empty j
+		#print(adj)							# return adj
 		#print("\n")
 		for a in range(len(adj)):
 			for b in range(len(adj[a])):
