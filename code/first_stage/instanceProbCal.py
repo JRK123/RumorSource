b484900b4dbb83e3c2496d7c6444f124172771f4
ganeshlandge <ganeshlandge936@gmail.com>
2019-03-30 13:49:42 +0530

    seperated instance probability function from twoStage.py

---
+++ code/first_stage/instanceProbCal.py
@@ -0,0 +1,20 @@
+#------------------CALCULATES PROBABILITY OF EACH INSTANCE----------------
+def prob_cal():  
+	W = [[] for i in range(4)]
+	for i in range(4):
+		with open('./../Create_Instance/instance' + str(i+1) + '.txt') as file:
+			arr = file.readlines()
+			for j in range(0,len(arr)):
+				src, dest, wgt = arr[j].split(" ")
+				W[i].append(float(wgt))
+	
+	probGraph = []
+	prod = 1
+	for i in range(4):
+		for j in range(0,len(W[i])):
+			prod = prod * W[i][j]
+		probGraph.append(float(prod))
+	
+	#print(probGraph)
+	return probGraph
+
